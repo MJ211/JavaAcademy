b247b7ea06b940eea9ee7890534286aea1278888
MJ211 <30292502+MJ211@users.noreply.github.com>
2020-09-29 20:26:44 +0200

    Add code from chapter 3

--- preliminaryTasks/codingWithKentBeck/KentBeckCoding/src/main/java/Dollar.java
+++ preliminaryTasks/codingWithKentBeck/KentBeckCoding/src/main/java/Dollar.java
@@ -4,8 +4,13 @@ public class Dollar {
         this.amount = amount;
     }
 
-    Dollar times(int multiplier) {
+    public Dollar times(int multiplier) {
         return new Dollar(amount *multiplier);
     }
 
+    @Override
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar) object;
+        return this.amount == dollar.amount;
+    }
 }
