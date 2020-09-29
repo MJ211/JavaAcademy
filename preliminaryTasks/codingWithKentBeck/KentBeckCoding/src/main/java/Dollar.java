47b401b2cef699eb2e535aafcf8c9278fa7b57ab
MJ211 <30292502+MJ211@users.noreply.github.com>
2020-09-29 22:01:12 +0200

    [KentBeckCoding] Add code from chapter 8

--- preliminaryTasks/codingWithKentBeck/KentBeckCoding/src/main/java/Dollar.java
+++ preliminaryTasks/codingWithKentBeck/KentBeckCoding/src/main/java/Dollar.java
@@ -3,7 +3,8 @@ public class Dollar extends Money {
         this.amount = amount;
     }
 
-    public Dollar times(int multiplier) {
-        return new Dollar(amount *multiplier);
+    @Override
+    public Money times(int multiplier) {
+        return new Dollar(amount * multiplier);
     }
 }
--- preliminaryTasks/codingWithKentBeck/KentBeckCoding/src/main/java/Money.java
+++ preliminaryTasks/codingWithKentBeck/KentBeckCoding/src/main/java/Money.java
@@ -1,9 +1,16 @@
-public class Money {
+public abstract class Money {
     protected int amount;
+    public abstract Money times(int multiplier);
 
     @Override
     public boolean equals(Object object) {
         Money dollar = (Money) object;
         return this.amount == dollar.amount && this.getClass().equals(object.getClass());
+    }
+    public static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+    public static Money franc(int amount) {
+        return new Franc(amount);
     }
 }
